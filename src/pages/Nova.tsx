29ba143308c3a9479f482e9d860a55f7554ba731
Carlos Eduardo Lourenço <nice.ice312@gmail.com>
2021-11-07 13:22:13 -0300

    Criação do componente da barra lateral

---
+++ src/pages/Nova.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Nova = () => {
+  return (
+    <>
+      <h1>Nova Página</h1>
+    </>
+  )
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,13 +2,17 @@
 // yarn add -D @types/react-router-dom
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { Nova } from '../pages/Nova';
 import { Test } from '../pages/Test';
 
 export const Routes: React.FC = () => {
 
   return(
-    <Switch>
-      <Route component={Test} path='/' exact/>
-    </Switch>
+    <>
+      <Switch>
+        <Route component={Test} path='/' exact/>
+        <Route component={Nova} path='/nova' exact />
+      </Switch>
+    </>
   )
 }
