a09b60dd9671a943071fa41c5351aa4c54720a97
LorenaDM <lorena.dmfacef@gmail.com>
2021-11-08 09:47:51 -0300

    btns prontos

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import {Button} from '../styles'
+import {Button2} from '../styles'
+
 export const Test = () => {
   return (
     <>
       <h1>Página para testes dos componentes</h1>
       <Button>Entrar</Button>
+      
+      <Button2>Cadastrar Membros</Button2>
+      <Button2>Visualizar Cadastros</Button2>
+      
+      <Button2>Menu Financeiro</Button2>
+      
+      <Button2>Cadastrar Compras</Button2>
+      <Button2>Visualizar Compras Por Membro</Button2>
+      
+      <Button2>Visualizar Vencimento Do Registro</Button2>
+      
+      <Button2>Visualizar Vencimento Das Mensalidades</Button2>
 
+      
 
 
 
--- src/styles/index.tsx
+++ src/styles/index.tsx
@@ -19,18 +19,45 @@ export const Text = styled.span`
 `;
 
 export const Button = styled.button`
-  padding: 12px;
-  border: 0;
-  background: #FFEA34;
+  font-size: 16px;
+  color: #fff;
+  width: 80px;
+  height: 45px;
+  border: none;
+  border-radius: 10px;
+  background: #000;
+  margin: 10px;
   
 
   &:hover {
-    opacity: 0.5;
+    transition: 0.4s;
+    cursor: pointer;
+    font-size: 22px;
   }
 
 `;
 
 export const Button2 = styled.button`
+  font-size: 32px;
+  width: 500px;
+  height: 100px;
+  border: none;
+  border-radius: 10px;
+  background: #FFEA34;
+  margin: 10px;
+  
+
+  &:hover {
+    transition: 0.4s;
+    cursor: pointer;
+    background: #FDE301;
+  }
+
+`;
+
+
+
+/* export const Button3 = styled(Link)`
   padding: 12px;
   border: 0;
   background: #FFEA34;
@@ -40,18 +67,6 @@ export const Button2 = styled.button`
     opacity: 0.5;
   }
 
-`;
-
-export const Button3 = styled.Link`
-  padding: 12px;
-  border: 0;
-  background: #FFEA34;
-  
-
-  &:hover {
-    opacity: 0.5;
-  }
-
-`;
+`;  */
 
 //git push origin lorena
