2566523fe309cd5d70bb60e7777f73b9123a1715
Carlos Eduardo Lourenço <nice.ice312@gmail.com>
2021-11-20 09:22:24 -0300

    ok

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -9,21 +9,21 @@ export const GlobalStyle = createGlobalStyle`
       box-sizing: border-box;
     }
 `
-const Button = styled.button`
-  /* Adapt the colors based on primary prop */
-  background: ${props => props.primary ? "palevioletred" : "white"};
-  color: ${props => props.primary ? "white" : "palevioletred"};
+// const Button = styled.button`
+//   /* Adapt the colors based on primary prop */
+//   background: ${props => props.primary ? "palevioletred" : "white"};
+//   color: ${props => props.primary ? "white" : "palevioletred"};
 
-  font-size: 1em;
-  margin: 1em;
-  padding: 0.25em 1em;
-  border: 2px solid palevioletred;
-  border-radius: 3px;
-`;
+//   font-size: 1em;
+//   margin: 1em;
+//   padding: 0.25em 1em;
+//   border: 2px solid palevioletred;
+//   border-radius: 3px;
+// `;
 
-render(
-  <div>
-    <Button>Normal</Button>
-    <Button primary>Primary</Button>
-  </div>
-);
+// render(
+//   <div>
+//     <Button>Normal</Button>
+//     <Button primary>Primary</Button>
+//   </div>
+// );
